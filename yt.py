a890d300c2de880a9dcf6e6aca5b8e2c976c398e
joeri2001 <joeri2001@proton.me>
2024-05-02 10:41:57 +0200

    docker fix and script update

--- yt.py
+++ yt.py
@@ -1,9 +1,17 @@
+import os
 from pytube import YouTube
 
 links = [
-    'video url 1',
+    'URL HERE',
 ]
 
+# Create the "videos" folder if it doesn't exist
+if not os.path.exists('videos'):
+    os.makedirs('videos')
+
 for link in links:
-    yt = YouTube(link)
-    yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+    try:
+        yt = YouTube(link)
+        yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download('videos')
+    except Exception as e:
+        print(f"Error downloading video from {link}: {str(e)}")
